071dba32b7b5a766170b2793bc575440df09b098
Chris Kerbo <chris.kerbo@outlook.com>
2020-01-18 03:31:57 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -25,7 +25,7 @@ class Song < ActiveRecord::Base
 
   def note_contents=(content)
     content.each do |content|
-      self.note = Note.find_or_create_by(content: content)
+      self.notes << Note.find_or_create_by(content: content)
     end
   end
 
